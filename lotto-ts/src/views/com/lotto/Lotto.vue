25f3d68b7f8df6df1685bcf5a84efc40a09a34c9
jkk <jk.20200102@gmail.com>
2020-05-30 13:01:55 +0900

    Class based component 적용

--- lotto-ts/src/views/com/lotto/Lotto.vue
+++ lotto-ts/src/views/com/lotto/Lotto.vue
@@ -32,64 +32,71 @@
 <script lang="ts">
 import Vue from 'vue'
 import _ from 'lodash'
+import {Component} from 'vue-property-decorator'
+// import Component from 'vue-class-component'
 // import { mapGetters, mapMutations, mapActions } from 'vuex'
 // import VueNumericInput from 'vue-numeric-input'
 
 // import * as CommonUtils from '@/components/com/utils/CommonUtils'
-import lottoPragment from '@/views/com/lotto/LottoPragment.vue'
+import LottoPragment from '@/views/com/lotto/LottoPragment.vue'
 
-export default Vue.extend({
-  name: 'lotto',
+const name = 'Lotto'
+// 데코레이터를 이용한 컴포넌트 정의
+@Component({
+  name: name,
   components: {
-    lottoPragment,
-  },
-  data() {
-    return {
-      winNumbers: [0,0,0,0,0,0],
-      isShowConfig: false,
-      latelySize: 10,
-      // winNumbers: [],
-      weights: Array(45).fill(0).map((v, i) => i === 0 ? 1 : 0),    // (45) [0,0,...,0]
-      seeds: [],
-      selectedNumbers: [],
-      isKeepNumber: !true,
-    }
-  },
-  /**  */
-  created() {
-    console.log('--- created')
-  },
-  mounted() {
-    console.log('--- mounted')
-  },
-  updated() {
-    // console.log('--- updated')
-  },
-  /** */
-  methods: {
-    onInit() {
-      console.log('--- onInit')
-      this.winNumbers = [1,2,3,4,5,6]
-    },
-    onRecommand() {
-      console.log('--- onRecommand')
-    },
-    clickButton() {
-      console.log('--- clickButton')
-      this.winNumbers = [10,20,30,40,5,6]
-      // this.$refs.winNumbers.setWinNumbers([1,2,3,4,5,6])
-    },
-    // getBallType(num: number) {
-    //   return Math.floor((num - 1/* 1~10 */) / 10) + 1
-    // },
-  },
-  computed: {
-    documentTitle() {
-      return document.title
-    }
-  },
-  watch: {
-
-  },
+    LottoPragment,
+  }
 })
+export default class Lotto extends Vue {
+
+  /****************************************************************************
+   * 
+   ****************************************************************************/
+  winNumbers = [0,0,0,0,0,0]
+  isShowConfig = false
+  latelySize = 10
+  weights = Array(45).fill(0).map((v, i) => i === 0 ? 1 : 0)    // (45) [0,0,...,0]
+  seeds = []
+  selectedNumbers = []
+  isKeepNumber = !true
+
+  /****************************************************************************
+   * 
+   ****************************************************************************/
+  created() {
+    console.log(`${name} created`)
+  }
+  mounted() {
+    console.log(`${name} mounted`)
+  }
+
+  /****************************************************************************
+   * 
+   ****************************************************************************/
+  onInit() {
+    console.log('--- onInit')
+    this.winNumbers = [1,2,3,4,5,6]
+  }
+  onRecommand() {
+    console.log('--- onRecommand')
+  }
+  clickButton() {
+    console.log('--- clickButton')
+    this.winNumbers = [10,20,30,40,5,6]
+    // this.$refs.winNumbers.setWinNumbers([1,2,3,4,5,6])
+  }
+  // getBallType(num: number) {
+  //   return Math.floor((num - 1/* 1~10 */) / 10) + 1
+  // },
+  /****************************************************************************
+   * 
+   ****************************************************************************/
+  get documentTitle() {
+    return document.title
+  }
+  /****************************************************************************
+   * 
+   ****************************************************************************/
+}
 </script>

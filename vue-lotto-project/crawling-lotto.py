715a79f169b86106351a6939305342d13e479d4a
Team Master <team.hb2004@gmail.com>
2020-08-02 08:52:13 +0900

    922회 당첨번호 추가 등

---
+++ vue-lotto-project/crawling-lotto.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.parse
+
+print('Start...')
+url = 'https://dhlottery.co.kr/gameResult.do?method=byWin&wiselog=C_A_1_1'
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    round = soup.find('div', {'class': 'win_result'}).find('strong').text
+    # print(round)
+    all_spans = soup.find_all('span', {'class': 'ball_645'})
+    # print(all_spans)
+    numbers = []
+    for span in all_spans:
+        numbers.append(int(span.text))
+print(f'/* {round} */ {numbers}')
+print('End.')
